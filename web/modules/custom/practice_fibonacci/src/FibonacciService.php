7fe515676459c1f68e2265acd35a7b8c7879ee3e
George Papadas <gpapadas@peripc.gr>
2021-05-01 18:50:58 +0300

    Add a simple module to practice testing

---
+++ web/modules/custom/practice_fibonacci/src/FibonacciService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\practice_fibonacci;
+
+/**
+ * A service that lets us generate Fibonacci sequences.
+ */
+class FibonacciService {
+
+  protected $fibonacciSequence = [0, 1];
+
+  /**
+   * Generates certain ammount of Fibonacci numbers.
+   */
+  public function calcSomeFibos($numberOfNumbers) {
+
+    if (count($this->fibonacciSequence) == $numberOfNumbers) {
+      return $this->fibonacciSequence;
+    } else {
+      $this->fibonacciSequence = $this->getPreceding(1) + $this->getPreceding(2);
+      return $this->calcSomeFibos($numberOfNumbers);
+    }
+  }
+
+  /**
+   * Getting the preceding number.
+   */
+  private function getPreceding($preceding = 1) {
+    return $this->fibonacciSequence[count($this->fibonacciSequence - $preceding)];
+  }
+}
