e632b79b749fd7880d2824376e2a65b007376584
George Papadas <gpapadas@peripc.gr>
2021-05-03 19:15:58 +0300

    Made basic functionality / Go for details

---
+++ web/modules/custom/drupal_test/src/Controller/PermutationsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Drupal\drupal_test\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\Markup;
+
+/**
+ * Provides route responses for the Drupal Test module.
+ */
+class PermutationsController extends ControllerBase {
+  /**
+   * Returns a permutations page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function permutationsPage() {
+
+    // Gets the current user.
+    $current_user = \Drupal::currentUser()->getAccountName();
+
+    // Gets the permutation keyword from the configs of the module.
+    $config = $this->config('drupal_test.config_permutations');
+    $permutation_keyword = $config->get('keyword');
+    $perm_key_length = strlen($permutation_keyword);
+
+    // Save permutations.
+    $permutations = '';
+    $this->permute($permutation_keyword, 0, $perm_key_length - 1, $permutations);
+
+    $markup = '<p>' . 'Hello, ' . $current_user . '</p>';
+    $markup .= '<p>Value provided in the back-office is ' . $permutation_keyword . '</p><br/>';
+    $markup .= 'Here are all permutations:';
+
+    $element = [
+      '#markup' => Markup::create($markup),
+      'field_permutations' => [
+        '#type' => 'textarea',
+        '#rows' => '15',
+        '#attributes' => ['class' => ['textarea-half-width']],
+        '#value' => $permutations,
+      ]
+    ];
+
+    return $element;
+  }
+
+  private function permute($str, $l, $r, &$p) {
+    if ($l == $r) {
+      $p .= $str . "\n";
+    } else {
+      for ($i = $l; $i <= $r; $i++) {
+        $str = $this->swap($str, $l, $i);
+        $this->permute($str, $l + 1, $r, $p);
+        $str = $this->swap($str, $l, $i);
+      }
+    }
+  }
+
+  private function swap($a, $i, $j) {
+
+    // $temp;
+    $charArray = str_split($a);
+    $temp = $charArray[$i] ;
+    $charArray[$i] = $charArray[$j];
+    $charArray[$j] = $temp;
+
+    return implode($charArray);
+  }
+}
